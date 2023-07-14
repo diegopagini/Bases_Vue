3300f9f9504db1b1bb7684bde38066ca427368bd
Diego Pagini <diego.pagini@neoris.com>
2023-07-14 13:24:27 +0200

    hola mundo

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 /** @format */
 
 const app = Vue.createApp({
-	template: `
-    <h1>Hola Mundo</h1>
-    <h2>Desde app.js</h2>
-  `,
+	// template: `
+	//   <h1> Hola Mundo </h1>
+	//   <h2> desde app.js </h2>
+	// `,
 });
 
 app.mount('#myApp');
