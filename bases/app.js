f51e42b384e4c98c28d93b34c85e050e76a92980
Diego Pagini <diego.pagini@neoris.com>
2023-07-14 14:22:11 +0200

    v-for

---
+++ bases/app.js
@@ -0,0 +1,28 @@
+/** @format */
+
+const app = Vue.createApp({
+	/** Options API */
+	// template: `
+	//   <h1> Hola Mundo </h1>
+	//   <h2> desde app.js </h2>
+	// `,
+	data() {
+		return {
+			quote: "I'm Batman",
+			author: 'Bruce Wayne',
+		};
+	},
+	methods: {
+		changeQuote() {
+			this.author = 'Batman';
+		},
+		capitalize() {
+			this.quote = this.quote.toUpperCase();
+		},
+	},
+	watch: {},
+	/** Composition API */
+	setup() {},
+});
+
+app.mount('#myApp');
