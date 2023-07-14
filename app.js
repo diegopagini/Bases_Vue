be0493dec23b940403ae4867a75c6cebc07e7ec3
Diego Pagini <diego.pagini@neoris.com>
2023-07-14 13:40:02 +0200

    data

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 /** @format */
 
 const app = Vue.createApp({
+	/** Options API */
 	// template: `
 	//   <h1> Hola Mundo </h1>
 	//   <h2> desde app.js </h2>
 	// `,
+	data() {
+		return {
+			quote: "I'm Batman",
+			author: 'Bruce Wayne',
+		};
+	},
+	methods: {},
+	watch: {},
+	/** Composition API */
+	setup() {},
 });
 
 app.mount('#myApp');
